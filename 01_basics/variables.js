37ba4820d7383db2d8614a8d617efc84be806ff0
Arun Singh Dangi <arundangi053@gmail.com>
2025-07-05 23:59:40 +0000

    changes done

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,6 +1,18 @@
-accountId=2
-console.log(accountId);
-accountcity="Bengaluru"
-accountPassword="2111232"
-accountno="234567"
-console.table([accountId,accountPassword,accountcity,accountno]);
+const accountid=144566;
+let accountemail="arun@java.com";
+var accountpassword="arun123";
+accountcity="jaipur"
+
+
+// accountid=2      // const cannot be changed not allowed 
+accountemail="hc@hc.com"
+accountPassword="21223234"
+accountcity="Haldwani"
+
+
+// prefered not  to use var 
+// becouse of issue in block space and function space 
+
+
+// console.table[accountid,accountemail,accountPassword,accountcity]
+console.table([accountid,accountemail,accountPassword,accountcity])
