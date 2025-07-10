de512fef753289209872a6b9b1c4a6fca4727591
Arun Singh Dangi <arundangi053@gmail.com>
2025-07-10 03:36:24 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,30 @@
+let score="33abs"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)   // in Number "n" should be capital
+// console.log(typeof valueInNumber);//will give number due to type conversion 
+// console.log( valueInNumber);   // will give NaN   i.e not  a number because 33abs is mixture of str and num ie not a number 
+
+// //"33"=33
+// "33abc"=> NaN
+// true=>1  false=>0
+//null=>0  undefined => NaN
+
+
+
+
+let isLoggedin=1
+let booleanIsLoggedIn =Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+//1=>true  0=>false
+//  " "  ( empty string)=> false
+// "hitesh"  => true
+
+
+let somenunber=33
+let stringNumber=String(somenunber)
+console.log(stringNumber)  // will give 33 
+console.log(typeof stringNumber)   // type get changed as string 
