bb9271a162f738e11ef3fe4ead1ed76a6c054e95
Arun Singh Dangi <arundangi053@gmail.com>
2025-07-10 03:02:24 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,35 @@
+"use strict";   // use for writing new version of js code 
+
+
+//  alert(3+3)   //will cause an error as it is nodejs not browser 
+
+console.log(5+
+    5)      // code should be readable and efficient 
+
+
+console.log("hitesh")
+
+//datatype
+
+let name ="arun"   //string
+let age=20         //number
+let islogggedin= false  //boolean
+
+
+// number range =2 to power of 53
+//bigint
+//string can be used in " " or ' '
+//boolean  = either "true" or false
+//null=> stand alone value(representation of empty value)
+//undefined =>  now this time value is not assigned 
+//symbol =>unique 
+
+
+//object=instance of class 
+
+//typeof  =>give you the datatype of variable or other 
+
+console.log(typeof " arun")
+console.log(typeof age)
+console.log(typeof null )  // it will show the datatype  as  object 
+console.log(typeof undefined  )  //it will show the datatype  as  undefined
